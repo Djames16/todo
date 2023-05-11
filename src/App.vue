b9fdfb2c3b7718fb7764de3ed70adc69a61d219e
Djames16 <Djames@brooklynsteamcenter.org>
2023-05-11 14:49:13 -0400

    changes 2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import {ref} from 'vue'
-let todos = ref (['A','B','C'])
+let todos = ref ([])
 let newTodo = ref('')
 function AddTodo(){
-  alert(newTodo.value)
+  todos.value.push(newTodo.value)
+
+  newTodo.value =""
+}
+function deleteTodo(index){
+  todos.value.splice(index,1)
 }
 </script>
 
@@ -12,42 +17,17 @@ function AddTodo(){
 
   <h1>My Todo Application</h1>
 <ol>
-  <li v-for="todo in todos">
+  <li v-for="(todo, index) in todos">
+    <button @click="deleteTodo(index)">🚮</button>
     {{ todo }}
   </li>
 </ol>
 
-<input v-model="newTodo">
+<input v-model="newTodo" @keydown.enter="AddTodo">
 <button @click="AddTodo"> Add Todo</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 body  {
     background: rgb(88,50,108);
 background: linear-gradient(117deg, rgb(244, 194, 194) 0%, rgb(137,207,240) 100%);

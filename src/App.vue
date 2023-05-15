f5714dbf3eabefb07258171767b6325cc2dbe8ac
Djames16 <Djames@brooklynsteamcenter.org>
2023-05-15 14:54:22 -0400

    Changes 4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import {ref} from 'vue'
-let todos = ref ([])
+import {ref, watch} from 'vue'
+let todos = ref (JSON.parse(window.localStorage.getItem('todos')))
 let newTodo = ref('')
+watch(todos, function(value){
+  window.localStorage.setItem('todos', JSON.stringify(value))
+  }, {deep: true})
 function AddTodo(){
   todos.value.push({
     text: newTodo.value,
@@ -38,5 +41,37 @@ background: linear-gradient(117deg, rgb(244, 194, 194) 0%, rgb(137,207,240) 100%
 font-family: 'Bruno Ace SC', cursive;
     text-align: center;
     min-height: 100vh;
+}
+
+input[type="checkbox"]{
+  appearance: none;
+  -webkit-appearance: none;
+  height: 20px;
+  width:20px;
+  background-color: #d5d5d5;
+  cursor:pointer;
+  display:inline-flex;
+  align-items: center;
+  justify-content:center
+}
+
+input[type="checkbox"]:after{
+  font-family: "Font Awesome 5 Free";
+  font-weight: 900;
+  content:"\f00c";
+  font-size:20px;
+  color: (244, 194, 194);
+  display:none
+}
+input[type="checkbox"]:hover{
+  background-color: blanchedalmond;
+}
+
+input[type="checkbox"]:checked{
+  background-color: aquamarine;
+}
+
+input[type="checkbox"]:checked::after{
+  display: block;
 }
 </style>
